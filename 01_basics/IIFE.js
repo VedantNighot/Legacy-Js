5d0c325f7f6c880d0ab7a199f5c526f9c0522393
Vedant Nighot <vedantnighotzzz1@gmail.com>
2025-01-07 13:55:51 +0000

    IIFE

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,10 @@
+// Imediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+((name)=>{
+    // simple IIFE
+    console.log(`DB CONNECTED TWO ${name}`)
+})('Vedant')
---
+++ 01_basics/functions.js
@@ -0,0 +1,21 @@
+// const user = ()=>{
+//     name:"vedant";
+//     console.log(this.name);
+// }
+// user();
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2;
+// }
+
+// Implicit return
+// const addTwo = (num1,num2) => num1+num2;
+
+// mainly use for react:
+
+// const addTwo = (num1,num2) => (num1+num2);
+
+// return object
+// const addTwo = (num1,num2) => ({username:"vedant"})
+
+// console.log(addTwo(2,3));
