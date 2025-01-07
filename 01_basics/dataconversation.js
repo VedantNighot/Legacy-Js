c19d4cfc330c1bb0bdaebb6fa87268867cc182cd
Vedant Nighot <vedantnighotzzz1@gmail.com>
2025-01-07 10:04:38 +0000

    added data conversation

---
+++ 01_basics/dataconversation.js
@@ -0,0 +1,7 @@
+let name = "Vedant";
+let number = "33";
+let number2 = 22;
+let number3 = Number(number);
+let number4 = String(number2);
+console.log(typeof number3); 
+console.log(typeof number4);
