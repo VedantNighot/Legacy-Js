5542fe50a038799b25b26e19d850ef926c0a9df0
Vedant Nighot <vedantnighotzzz1@gmail.com>
2025-01-07 10:58:58 +0000

    arrays,func,object

---
+++ 01_basics/arrays.js
@@ -0,0 +1,20 @@
+// Arrays, Object, Functions
+// Arrays:
+    const heroes = ["captain-american","iron-man","black-wirdow"];
+// objects
+    let detais = 
+    {
+        name:"vedant",
+        age:18,
+        college:"sinhgad",
+        Roll_number:61,
+        division:"B"
+    }
+    console.log(detais.age);
+
+// Functions
+const myFunction = function(){
+    console.log("hello vedant");
+}
+
+myFunction();
